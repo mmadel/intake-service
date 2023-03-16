86c2b550c0857ec1cb65ec0a2d30f14a43222193
mmadel <mohammed.adel82@gmail.com>
2023-03-16 19:46:18 +0200

    init commit

---
+++ src/test/java/com/cob/salesforce/CobSalesForceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cob.salesforce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CobSalesForceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
