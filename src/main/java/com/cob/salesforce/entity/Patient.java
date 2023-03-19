0519542853f7295198d7f820e960b7f023d50459
mmadel <mohammed.adel82@gmail.com>
2023-03-19 13:01:51 +0200

    add  patient to entity package

---
+++ src/main/java/com/cob/salesforce/entity/Patient.java
@@ -0,0 +1,62 @@
+package com.cob.salesforce.entity;
+
+import com.cob.salesforce.enums.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+
+@Entity(name = "patient")
+@Getter
+@Setter
+public class Patient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name="name")
+    private String name;
+
+    @Column(name = "date_of_birth")
+    private long dateOfBirth;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "gender")
+    private Gender gender;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "phone_type")
+    private PhoneType phoneType;
+    @Column(name="phone")
+    private String phone;
+
+    @Column(name = "id_effective_from")
+    private long idEffectiveFrom;
+    @Column(name = "id_effective_to")
+    private long idEffectiveTo;
+    @Column(name="email")
+    private String email;
+    @Column(name="emergency_name")
+    private String emergencyName;
+    @Column(name="emergency_phone")
+    private String emergencyPhone;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "employment_status")
+    private EmploymentStatus employmentStatus;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "patient_source_type")
+    private PatientSourceType patientSourceType;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "insurance_worker_type")
+    private InsuranceWorkerType insuranceWorkerType;
+
+    @Column(name = "Physical_therapy")
+    private Boolean PhysicalTherapy;
+
+
+}
