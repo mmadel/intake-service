92f3cbaa2198d79f6ca7d388d714a50b9a75f84a
mmadel <mohammed.adel82@gmail.com>
2023-03-29 14:23:48 +0200

    make NPI in Query ANDED

--- src/main/java/com/cob/salesforce/repositories/patient/PatientDoctorSourceRepository.java
+++ src/main/java/com/cob/salesforce/repositories/patient/PatientDoctorSourceRepository.java
@@ -10,7 +10,7 @@ import java.util.List;
 public interface PatientDoctorSourceRepository extends PagingAndSortingRepository<PatientDoctorSource, Long> {
 
     @Query("SELECT ds FROM patient_doctor_source ds   WHERE (:name is null or ds.name  =:name)" +
-            "OR (:npi is null or ds.npi  =:npi) " +
+            "AND (:npi is null or ds.npi  =:npi) " +
             "AND ((:dateFrom is null or ds.createdAt >= :dateFrom) AND (:dateTo is null or ds.createdAt < :dateTo) )")
     List<PatientDoctorSource> findDoctor(@Param("name") String name
             , @Param("npi") String npi
