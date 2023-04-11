fb4091221fd2f2f0ae8421ab08b15189d363107b
mmadel <mohammed.adel82@gmail.com>
2023-04-11 16:24:34 +0200

    create SecurityConfiguration to impl security filter and add required beans like decoder and encoder

---
+++ src/main/java/com/cob/salesforce/configuration/SecurityConfiguration.java
@@ -0,0 +1,81 @@
+package com.cob.salesforce.configuration;
+
+import com.cob.salesforce.enums.admin.UserRole;
+import com.cob.salesforce.services.admin.user.JpaUserDetailsService;
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+    private static final String SCOPE = "SCOPE_";
+    @Autowired
+    JpaUserDetailsService jpaUserDetailsService;
+
+    @Bean
+    public AuthenticationManager authenticationManager() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(jpaUserDetailsService);
+        return new ProviderManager(authProvider);
+    }
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .antMatchers("/api/auth/token").permitAll()
+                        .antMatchers("/user").hasAuthority(SCOPE + UserRole.USER.label)
+                        .antMatchers("/admin").hasAuthority(SCOPE + UserRole.ADMIN.label)
+                        .anyRequest().authenticated()
+
+                )
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .userDetailsService(jpaUserDetailsService)
+                .build();
+    }
+
+    @Bean
+    public RsaKeyProperties rsaKeys() {
+        return new RsaKeyProperties();
+    }
+
+    @Bean
+    JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withPublicKey(rsaKeys().publicKey).build();
+    }
+
+    @Bean
+    JwtEncoder jwtEncoder() {
+        JWK jwk = new RSAKey.Builder(rsaKeys().publicKey).privateKey(rsaKeys().privateKey).build();
+        JWKSource<SecurityContext> jwks = new ImmutableJWKSet(new JWKSet(jwk));
+        return new NimbusJwtEncoder(jwks);
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return  new BCryptPasswordEncoder();
+    }
+}
