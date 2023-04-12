6cc02ee1a433ba61204983421a5b1b6ffec67733
mmadel <mohammed.adel82@gmail.com>
2023-04-12 22:44:04 +0200

    expose access token and user id

--- src/main/java/com/cob/salesforce/controllers/AuthenticationController.java
+++ src/main/java/com/cob/salesforce/controllers/AuthenticationController.java
@@ -1,16 +1,19 @@
 package com.cob.salesforce.controllers;
 
 import com.cob.salesforce.models.admin.security.LoginRequest;
+import com.cob.salesforce.models.admin.security.LoginResponse;
+import com.cob.salesforce.models.admin.security.SecurityUser;
 import com.cob.salesforce.services.security.JWTGeneratorService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/auth")
 public class AuthenticationController {
@@ -19,12 +22,17 @@ public class AuthenticationController {
     @Autowired
     AuthenticationManager authenticationManager;
 
+    @Autowired
+    ModelMapper mapper;
 
     @PostMapping("/login")
-    public String generateToken(@RequestBody LoginRequest model) {
+    public ResponseEntity generateToken(@RequestBody LoginRequest model) {
         Authentication authentication = authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(
                         model.getUserName(), model.getPassword()));
-        return jwtGeneratorService.generateToken(authentication);
+        return new ResponseEntity(LoginResponse.builder()
+                .accessToken(jwtGeneratorService.generateToken(authentication))
+                .userId(((SecurityUser) authentication.getPrincipal()).user.getId())
+                .build(), HttpStatus.OK);
     }
 }
---
+++ src/main/java/com/cob/salesforce/models/admin/security/LoginResponse.java
@@ -0,0 +1,14 @@
+package com.cob.salesforce.models.admin.security;
+
+import com.cob.salesforce.models.admin.user.UserModel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Builder
+public class LoginResponse {
+    private String accessToken;
+    private Long  userId;
+}
