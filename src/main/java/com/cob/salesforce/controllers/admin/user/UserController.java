6bdda83be3193c8a4edbfce2449181cd0d232092
mmadel <mohammed.adel82@gmail.com>
2023-04-12 22:48:00 +0200

    expose getLoggedIn api

--- src/main/java/com/cob/salesforce/controllers/admin/user/UserController.java
+++ src/main/java/com/cob/salesforce/controllers/admin/user/UserController.java
@@ -1,8 +1,10 @@
 package com.cob.salesforce.controllers.admin.user;
 
+import com.cob.salesforce.models.admin.security.CurrentLoggInUser;
 import com.cob.salesforce.models.admin.user.UserModel;
 import com.cob.salesforce.services.admin.user.UserCreatorService;
 import com.cob.salesforce.services.admin.user.UserFinderService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +20,8 @@ public class UserController {
     @Autowired
     UserFinderService finder;
 
+    @Autowired
+    ModelMapper mapper;
     @PostMapping
     @ResponseBody
     public ResponseEntity create(@RequestBody UserModel model) {
@@ -34,5 +38,10 @@ public class UserController {
     @ResponseBody
     public ResponseEntity getById(@PathVariable Long userId) {
         return new ResponseEntity(finder.getById(userId), HttpStatus.OK);
+    }
+    @GetMapping(path = "/find/loggedIn/{userId}")
+    @ResponseBody
+    public ResponseEntity getLoggedIn(@PathVariable String userId) {
+        return new ResponseEntity(mapper.map(finder.getById(Long.valueOf(userId)), CurrentLoggInUser.class), HttpStatus.OK);
     }
 }
