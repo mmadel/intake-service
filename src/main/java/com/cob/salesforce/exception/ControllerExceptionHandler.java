1fad3e6cf186e69fca3395f1ca792d6cc87a2c33
mmadel <mohammed.adel82@gmail.com>
2023-05-28 11:12:17 +0300

    add ControllerAdvice

---
+++ src/main/java/com/cob/salesforce/exception/ControllerExceptionHandler.java
@@ -0,0 +1,8 @@
+package com.cob.salesforce.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
+}
