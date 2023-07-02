6882cd9a1e80b93f9336e2d5db846de78b283e90
mmadel <mohammed.adel82@gmail.com>
2023-07-03 01:26:27 +0300

    add upload image to permitted request

--- src/main/java/com/cob/salesforce/configuration/SecurityConfiguration.java
+++ src/main/java/com/cob/salesforce/configuration/SecurityConfiguration.java
@@ -54,6 +54,7 @@ public class SecurityConfiguration {
                         .antMatchers("/user/find/loggedIn/**").permitAll()
                         .antMatchers("/requires/fields/**").permitAll()
                         .antMatchers("/questionnaire/**").permitAll()
+                        .antMatchers(HttpMethod.POST,"/patient/upload").permitAll()
                         .antMatchers("/agreement").permitAll()
                         .antMatchers(HttpMethod.POST,"/patient/create").permitAll()
                         .antMatchers("/find/patient**").hasAnyAuthority(SCOPE + UserRole.USER.label,SCOPE + UserRole.ADMIN.label)
