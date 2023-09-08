dd01756c8b9319385854230755d9739bbe8cc3d4
madel <mohammed.adel.abo.foutouh@gmail.com>
2023-09-08 03:15:12 +0300

    fix performance issue in db when query  the audit

--- src/main/java/com/cob/salesforce/configuration/AuditConfiguration.java
+++ src/main/java/com/cob/salesforce/configuration/AuditConfiguration.java
@@ -1,10 +1,14 @@
 package com.cob.salesforce.configuration;
 
 import com.cob.salesforce.audit.AuditorAwareImpl;
+import org.hibernate.envers.AuditReader;
+import org.hibernate.envers.AuditReaderFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import javax.persistence.EntityManagerFactory;
 
 @Configuration
 @EnableJpaAuditing(auditorAwareRef = "auditorProvider")
@@ -12,5 +16,15 @@ public class AuditConfiguration {
     @Bean
     public AuditorAware<String> auditorProvider() {
         return new AuditorAwareImpl();
+    }
+    private final EntityManagerFactory entityManagerFactory;
+
+    AuditConfiguration(EntityManagerFactory entityManagerFactory) {
+        this.entityManagerFactory = entityManagerFactory;
+    }
+
+    @Bean
+    AuditReader reader() {
+        return AuditReaderFactory.get(entityManagerFactory.createEntityManager());
     }
 }
--- src/main/java/com/cob/salesforce/services/audit/InsuranceCompanyAuditService.java
+++ src/main/java/com/cob/salesforce/services/audit/InsuranceCompanyAuditService.java
@@ -1,25 +1,16 @@
 package com.cob.salesforce.services.audit;
 
-import com.cob.salesforce.entity.admin.ClinicEntity;
 import com.cob.salesforce.entity.admin.InsuranceCompanyEntity;
 import com.cob.salesforce.entity.audit.CustomRevisionEntity;
 import com.cob.salesforce.models.admin.audit.AuditModel;
-import org.hibernate.envers.AuditReader;
-import org.hibernate.envers.AuditReaderFactory;
 import org.hibernate.envers.RevisionType;
-import org.hibernate.envers.query.AuditEntity;
-import org.hibernate.envers.query.AuditQuery;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.persistence.EntityManagerFactory;
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class InsuranceCompanyAuditService extends AuditService {
-    @Autowired
-    EntityManagerFactory factory;
 
     @Override
     public List<AuditModel> getByEntity(Class entity) throws ClassNotFoundException {
