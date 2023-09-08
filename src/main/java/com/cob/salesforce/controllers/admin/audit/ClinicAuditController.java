139c5b3bf48651d1e4e216cd391ab04d57eccdc4
madel <mohammed.adel.abo.foutouh@gmail.com>
2023-09-08 04:04:56 +0300

    temp fix

--- src/main/java/com/cob/salesforce/controllers/admin/audit/ClinicAuditController.java
+++ src/main/java/com/cob/salesforce/controllers/admin/audit/ClinicAuditController.java
@@ -2,6 +2,7 @@ package com.cob.salesforce.controllers.admin.audit;
 
 import com.cob.salesforce.entity.admin.ClinicEntity;
 import com.cob.salesforce.services.audit.ClinicAuditService;
+import com.cob.salesforce.services.audit.Temp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,18 +11,21 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping(value = "/audit/clinic")
 public class ClinicAuditController {
-    @Autowired
+   /* @Autowired
     ClinicAuditService auditService;
+    */
+    @Autowired
+    Temp temp;
 
     @GetMapping(path = "/retrieve/uuid/{uuid}")
     @ResponseBody
     public ResponseEntity retrieveByEntityAndUUID(@PathVariable String uuid)  {
-        return new ResponseEntity(auditService.getByEntityAndUUID(ClinicEntity.class, uuid), HttpStatus.OK);
+        return new ResponseEntity(temp.getByEntityAndUUID(ClinicEntity.class, uuid), HttpStatus.OK);
     }
 
     @GetMapping(path = "/retrieve")
     @ResponseBody
     public ResponseEntity retrieveByUUID() throws ClassNotFoundException {
-        return new ResponseEntity(auditService.getByEntity(ClinicEntity.class), HttpStatus.OK);
+        return new ResponseEntity(temp.getByEntity(ClinicEntity.class), HttpStatus.OK);
     }
 }
