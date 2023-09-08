b07e12e353cfb469d9a6dced4f2fe3be0fb1c9b0
madel <mohammed.adel.abo.foutouh@gmail.com>
2023-09-08 04:20:35 +0300

    temp fix-insurance company

--- src/main/java/com/cob/salesforce/controllers/admin/audit/InsuranceCompanyAuditController.java
+++ src/main/java/com/cob/salesforce/controllers/admin/audit/InsuranceCompanyAuditController.java
@@ -4,6 +4,7 @@ import com.cob.salesforce.entity.admin.ClinicEntity;
 import com.cob.salesforce.entity.admin.InsuranceCompanyEntity;
 import com.cob.salesforce.services.audit.ClinicAuditService;
 import com.cob.salesforce.services.audit.InsuranceCompanyAuditService;
+import com.cob.salesforce.services.audit.Tempa;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,16 +15,18 @@ import org.springframework.web.bind.annotation.*;
 public class InsuranceCompanyAuditController {
     @Autowired
     InsuranceCompanyAuditService auditService;
+    @Autowired
+    Tempa tempa;
 
     @GetMapping(path = "/retrieve/uuid/{uuid}")
     @ResponseBody
     public ResponseEntity retrieveByEntityAndUUID(@PathVariable String uuid) {
-        return new ResponseEntity(auditService.getByEntityAndUUID(InsuranceCompanyEntity.class, uuid), HttpStatus.OK);
+        return new ResponseEntity(tempa.getByEntityAndUUID(InsuranceCompanyEntity.class, uuid), HttpStatus.OK);
     }
 
     @GetMapping(path = "/retrieve")
     @ResponseBody
     public ResponseEntity retrieveByUUID() throws ClassNotFoundException {
-        return new ResponseEntity(auditService.getByEntity(InsuranceCompanyEntity.class), HttpStatus.OK);
+        return new ResponseEntity(tempa.getByEntity(InsuranceCompanyEntity.class), HttpStatus.OK);
     }
 }
---
+++ src/main/java/com/cob/salesforce/services/audit/Tempa.java
@@ -0,0 +1,54 @@
+package com.cob.salesforce.services.audit;
+
+import com.cob.salesforce.entity.admin.InsuranceCompanyEntity;
+import com.cob.salesforce.entity.audit.CustomRevisionEntity;
+import com.cob.salesforce.models.admin.audit.AuditModel;
+import org.hibernate.envers.AuditReader;
+import org.hibernate.envers.AuditReaderFactory;
+import org.hibernate.envers.RevisionType;
+import org.hibernate.envers.query.AuditEntity;
+import org.hibernate.envers.query.AuditQuery;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class Tempa {
+    @Autowired
+    private EntityManager entityManager;
+
+    public List<AuditModel> getByEntity(Class entity) {
+        return getByEntityAndUUID(entity, null);
+    }
+    @Transactional
+    public List<AuditModel> getByEntityAndUUID(Class entity, String UUID) {
+        AuditReader auditReader = AuditReaderFactory.get(entityManager);
+        AuditQuery auditQuery = auditReader.createQuery()
+                .forRevisionsOfEntity(entity, false, false)
+                .addOrder(AuditEntity.revisionProperty("timestamp").asc());
+        if (UUID != null)
+            auditQuery.add(AuditEntity.revisionProperty("uuid").eq(UUID));
+        List insuranceCompanyQueryResult = auditQuery.getResultList();
+        List<AuditModel> result = new ArrayList<>();
+        for (Object item : insuranceCompanyQueryResult) {
+            InsuranceCompanyEntity entityClass = (InsuranceCompanyEntity) ((Object[]) item)[0];
+            CustomRevisionEntity CustomRevisionEntity = (com.cob.salesforce.entity.audit.CustomRevisionEntity) ((Object[]) item)[1];
+            Integer versionId = CustomRevisionEntity.getId();
+
+            InsuranceCompanyEntity insuranceCompanyVersion = (InsuranceCompanyEntity) auditReader.find(entity, entityClass.getId(), versionId);
+            RevisionType revisionType = (RevisionType) ((Object[]) item)[2];
+            result.add(AuditModel.builder()
+                    .revisionDate(CustomRevisionEntity.getTimestamp())
+                    .uuid(CustomRevisionEntity.getUuid())
+                    .revisionType(revisionType)
+                    .entity(insuranceCompanyVersion)
+                    .entityName(((Object[]) item)[0].getClass().getSimpleName())
+                    .build());
+        }
+        return result;
+    }
+}
