c43f348019949a2c04a283c8ccf7e95e369afd52
madel <mohammed.adel.abo.foutouh@gmail.com>
2023-09-24 14:30:36 +0300

    remove  patient document entity and its repo

--- src/main/java/com/cob/salesforce/repositories/intake/PatientDocumentRepositoryNew.java
+++
@@ -1,7 +0,0 @@
-package com.cob.salesforce.repositories.intake;
-
-import com.cob.salesforce.entity.intake.PatientDocumentEntity;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PatientDocumentRepositoryNew extends CrudRepository<PatientDocumentEntity, Long> {
-}
