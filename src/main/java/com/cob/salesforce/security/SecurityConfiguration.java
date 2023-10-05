f59f2ec30d9583a69ff110291f25067845fd0305
madel <mohammed.adel.abo.foutouh@gmail.com>
2023-10-05 18:15:49 +0300

    fix

--- src/main/java/com/cob/salesforce/security/SecurityConfiguration.java
+++ src/main/java/com/cob/salesforce/security/SecurityConfiguration.java
@@ -40,7 +40,7 @@ public class SecurityConfiguration {
                         .antMatchers("/insurance/company/find","/patient/create","/agreement","/reports/generator/pdf/patientId/**").hasAnyRole("normal")
                         .antMatchers("/dashboard/**", "/clinic/**",
                                 "/insurance/company/**", "/reports/recommendation/**",
-                                "/reports/generator/**","reports/generator/pdf/patientId/**","/audit/**").hasRole("administrator")
+                                "/reports/generator/**","reports/generator/pdf/patientId/**","/audit/**","/insurance/company/find").hasRole("administrator")
                         .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 -> {
